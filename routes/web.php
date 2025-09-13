ed31e5f6ddcc91bbf9a951ad516a6dea851cea20
shayankhannn <shayannkhannn007@gmail.com>
2025-09-13 00:30:31 -0700

    route::resource

--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,25 @@ use Illuminate\Support\Facades\Route;
 //home
 Route::view('/','home');
 
-Route::controller(JobController::class)->group(function (){
-// index job
-Route::get('/jobs','index' );
-// create job
-Route::get('/jobs/create','create');
-// store job
-Route::post('/jobs','store' );
-// show job
-Route::get('/jobs/{job}','show' );
-// edit job
-Route::get('/jobs/{job}/edit','edit');
-// Update job
-Route::patch('/jobs/{job}', 'update');
-// delete job
-Route::delete('/jobs/{job}','destroy');
+// Route::controller(JobController::class)->group(function (){
+// // index job
+// Route::get('/jobs','index' );
+// // create job
+// Route::get('/jobs/create','create');
+// // store job
+// Route::post('/jobs','store' );
+// // show job
+// Route::get('/jobs/{job}','show' );
+// // edit job
+// Route::get('/jobs/{job}/edit','edit');
+// // Update job
+// Route::patch('/jobs/{job}', 'update');
+// // delete job
+// Route::delete('/jobs/{job}','destroy');
 
-});
+// });`
+
+Route::resource('jobs',JobController::class);
 
 //contact
 Route::view('/contact', 'contact');
