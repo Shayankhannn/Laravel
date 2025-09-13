f74a2828115e7cdb9bb27fbafd2b2267b4ac1534
shayankhannn <shayannkhannn007@gmail.com>
2025-09-13 00:21:47 -0700

    Route::view

--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,23 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-    // $jobs = Job::all();
-    // dd($jobs[0]->title);
-});
 
+//home
+Route::view('/','home');
 // index job
 Route::get('/jobs',[JobController::class,'index'] );
-
 // create job
 Route::get('/jobs/create',[JobController::class,'create']);
-
 // store job
 Route::post('/jobs',[JobController::class,'store'] );
-
 // show job
 Route::get('/jobs/{job}',[JobController::class,'show'] );
-
 // edit job
 Route::get('/jobs/{job}/edit',[JobController::class,'edit']);
-
 // Update job
 Route::patch('/jobs/{job}', [JobController::class,'update']);
-
 // delete job
-
 Route::delete('/jobs/{job}',[JobController::class,'destroy']);
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+//contact
+Route::view('/contact', 'contact');
