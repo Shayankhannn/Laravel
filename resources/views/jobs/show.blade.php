2b735a7e6a8b90f35860aacdeb76d884496cfd62
shayankhannn <shayannkhannn007@gmail.com>
2025-09-21 00:26:32 -0700

    can cannot authorization

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -6,12 +6,12 @@
     <h2>{{$job->title}}</h2>
     <p>{{$job->salary}} per year</p>
 
-@can('edit-job',$job)
+@cannot('edit-job',$job)
     
 <p class="mt-5">
 <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
 </p>
-@endcan
+@endcannot  
 
 
 </x-layout>
